a1b7e97865de8c9dc182f3095ed71f309ac1139c
Test Testov <RQDerz@yandex.ru>
2022-06-22 19:44:42 +0300

    MainActivity переконвертирован в kotlin

---
+++ Android_Kotlin_1/app/src/main/java/com/example/lesson01app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lesson01app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lesson01app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
