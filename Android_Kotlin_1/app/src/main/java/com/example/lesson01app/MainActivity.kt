69d7a37e07b5d3e2e57911148c4b1f438d23e659
Test Testov <RQDerz@yandex.ru>
2022-06-22 22:03:10 +0300

    Добавлена кнопка, по нажатию на которую, создается массив из двух случайных чисел. В первый текстВью посредством foreach выводится массив, во второй - второе число количество раз равное первому числу посредством repeat. В третий пошаговое движение от первого числа ко второму посредстовм for.

--- Android_Kotlin_1/app/src/main/java/com/example/lesson01app/MainActivity.kt
+++ Android_Kotlin_1/app/src/main/java/com/example/lesson01app/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.lesson01app.R
+import java.lang.StringBuilder
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,18 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         data class Cat(val name: String, val weight: Int)
+
         val cat = Cat("Усатый", 5)
 
-       /* var dog = object {
-            var name = "Слюнявый"
-            var weight = 20
-        }*/
+        /* var dog = object {
+             var name = "Слюнявый"
+             var weight = 20
+         }*/
 
         val newCat = cat.copy(name = "Усатый")
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener(object : View.OnClickListener {
-            @SuppressLint("ResourceAsColor")
             override fun onClick(v: View) {
                 if (button.text == "button") {
                     button.setText("Кнопка")
@@ -35,6 +37,37 @@ class MainActivity : AppCompatActivity() {
 
                 findViewById<TextView>(R.id.cat_name).setText(newCat.name)
                 findViewById<TextView>(R.id.cat_weight).setText(newCat.weight.toString())
+            }
+        })
+
+        val buttonCycles = findViewById<Button>(R.id.button_cycles)
+        buttonCycles.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                val foreachTV = findViewById<TextView>(R.id.foreach_textview)
+
+                val numbers = List(2) { Random.nextInt(0, 99) }
+                var sg = ""
+
+                numbers.forEach {
+                    sg += it.toString() + " "
+                }
+                foreachTV.setText(sg)
+
+                sg = ""
+                repeat(numbers[0]){
+                    sg+= numbers[1].toString() + " "
+                }
+                findViewById<TextView>(R.id.repeat_textview).setText(sg)
+
+                sg = ""
+                if (numbers[0] < numbers[1]){
+                for (i in numbers[0]..numbers[1]){
+                    sg+= i.toString() + " "
+                }} else {
+                    for (i in numbers[0] downTo numbers[1]){
+                        sg+= i.toString() + " "
+                }}
+                findViewById<TextView>(R.id.fori_textview).setText(sg)
             }
         })
     }
