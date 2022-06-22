dedbd12ed0264860761af4e1c96a36d8d3a24f3d
Test Testov <RQDerz@yandex.ru>
2022-06-22 20:31:36 +0300

    Создан data class Cat со свойствами name: String, weight: Int. Через код создается и инициилизируется экземпляр cat, который по нажатии на кнопку выводит свои свойства в два TextView.

--- Android_Kotlin_1/app/src/main/java/com/example/lesson01app/MainActivity.kt
+++ Android_Kotlin_1/app/src/main/java/com/example/lesson01app/MainActivity.kt
@@ -5,21 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.example.lesson01app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        data class Cat(val name: String, val weight: Int)
+        val cat = Cat("Усатый", 5)
+
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener(object : View.OnClickListener {
             @SuppressLint("ResourceAsColor")
             override fun onClick(v: View) {
-                if (button.text == "Button") {
+                if (button.text == "button") {
                     button.setText("Кнопка")
                 } else {
-                    button.setText("Button")
+                    button.setText("button")
                 }
+                findViewById<TextView>(R.id.cat_name).setText(cat.name)
+                findViewById<TextView>(R.id.cat_weight).setText(cat.weight.toString())
             }
         })
     }
