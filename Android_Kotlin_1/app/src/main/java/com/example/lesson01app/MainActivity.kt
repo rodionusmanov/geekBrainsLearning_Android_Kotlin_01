baae52a790e889e0367aeed01e0f26e22ba12dfe
Test Testov <RQDerz@yandex.ru>
2022-06-22 20:08:00 +0300

    Добавлена кнопка. На нее повешен кликЛистенер, по нажатии переводящий название кнопки на другой язык.

--- Android_Kotlin_1/app/src/main/java/com/example/lesson01app/MainActivity.kt
+++ Android_Kotlin_1/app/src/main/java/com/example/lesson01app/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.lesson01app
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.lesson01app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(object : View.OnClickListener {
+            @SuppressLint("ResourceAsColor")
+            override fun onClick(v: View) {
+                if (button.text == "Button") {
+                    button.setText("Кнопка")
+                } else {
+                    button.setText("Button")
+                }
+            }
+        })
     }
 }
